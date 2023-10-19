0c24380fd4e167aea0c01e786b062bd7a4626ab2
Cvin230 <148172796+Cvin230@users.noreply.github.com>
2023-10-19 18:01:14 +0000

    Create Login.jsx

---
+++ test/client/src/Components/Login.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+
+const Login = ({ onLogin }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('http://localhost:5000/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        onLogin(data.username);
+      } else {
+        alert(data.message); // Display error message if login fails
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>Username:</label>
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <div>
+        <label>Password:</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+export default Login;
