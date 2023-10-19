9fea74b721d439924442240daff5f54356ab329a
Cvin230 <148172796+Cvin230@users.noreply.github.com>
2023-10-19 23:14:00 +0000

    Update authenticationController.js

--- test/server/controllers/authenticationController.js
+++ test/server/controllers/authenticationController.js
@@ -4,7 +4,7 @@ const getUser  = async (req, res) => {
   const { username, password } = req.body;
 
   if (username == process.env.USERNAME && password === process.env.PASSWORD) {
-    res.json({ message: 'Login successful' });
+    res.json({ message: 'Login successful', username: username });
   } else {
     res.status(401).json({ message: 'Invalid credentials' });
   }
