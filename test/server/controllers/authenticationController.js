94aa57c49aafd6c6978413b39c85b9fcc39a56af
Cvin230 <148172796+Cvin230@users.noreply.github.com>
2023-10-19 21:36:25 +0000

    Update authenticationController.js

--- test/server/controllers/authenticationController.js
+++ test/server/controllers/authenticationController.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const getUser  = async (req, res) => {
   const { username, password } = req.body;
 
-  if (username === process.env.USERNAME && password === process.env.PASSWORD) {
+  if (username == process.env.USERNAME && password === process.env.PASSWORD) {
     res.json({ message: 'Login successful' });
   } else {
     res.status(401).json({ message: 'Invalid credentials' });
